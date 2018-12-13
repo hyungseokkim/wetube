25e5c5f928c3ae18776a864c25afa7675f781fe0
김형석 <hyungseokkim@gimhyeongseog-ui-MacBookPro.local>
2018-12-13 19:44:17 +0900

    firsttime

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+//request, response를 추가하여 callback을 만든다
+const handleHome = (req, res) => res.send('Hello from home');
+
+const handleProfile = (req,res) => res.send("You are on my profile");
+
+
+app.get("/", handleHome); // "/"으로 접속 시 handleHome실행
+
+app.get("/profile", handleProfile); // "/profile"로 접속 시 handleProfile실행
+
+app.listen(PORT, handleListening);
