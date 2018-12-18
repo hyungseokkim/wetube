f005de6b8b2042bb262b0989a5494cb196908ae3
김형석 <hyungseokkim@gimhyeongseog-ui-MacBookPro.local>
2018-12-18 14:15:38 +0900

    added middleware

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () =>
+ console.log(`Listening on: http://localhost:${PORT}`);
 //request, response를 추가하여 callback을 만든다
-const handleHome = (req, res) => res.send('Hello from home');
+const handleHome = (req, res) => 
+res.send('Hello from home');
 
-const handleProfile = (req,res) => res.send("You are on my profile");
+const handleProfile = (req,res) => 
+res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+    console.log('Between');
+    next();
+};
 
-app.get("/", handleHome); // "/"으로 접속 시 handleHome실행
+app.use(betweenHome); //middleware
+
+app.get("/", betweenHome, handleHome); // "/"으로 접속 시 handleHome실행
 
 app.get("/profile", handleProfile); // "/profile"로 접속 시 handleProfile실행
 
