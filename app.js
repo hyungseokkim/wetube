06e663b16ca1a1631a15246fa246d2541f5f71f9
김형석 <hyungseokkim@gimhyeongseog-ui-MacBookPro.local>
2018-12-22 17:25:40 +0900

    12.22.router추가

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {userRouter} from "./router";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
- console.log(`Listening on: http://localhost:${PORT}`);
 //request, response를 추가하여 callback을 만든다
 const handleHome = (req, res) => 
 res.send('Hello from home');
@@ -33,4 +30,6 @@ app.get("/", middleware, handleHome); // "/"으로 접속 시 handleHome실행
 
 app.get("/profile", handleProfile); // "/profile"로 접속 시 handleProfile실행
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter); //use를 사용하면 router의 해당 라우터를 다 사용하겠다.
+
+export default app;
--- init.js
+++ init.js
@@ -1,10 +1,4 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-
-const app = express();
+import app from "./app";
 
 const PORT = 4000;
 
@@ -13,20 +7,4 @@ const handleListening = () =>
 //request, response를 추가하여 callback을 만든다
 
 
-/* middleware */
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
-app.use(morgan("dev"));
-
-const middleware = (req, res, next) => {
-    res.send('not happening');
-};
-
-
-app.get("/", middleware, handleHome); // "/"으로 접속 시 handleHome실행
-
-app.get("/profile", handleProfile); // "/profile"로 접속 시 handleProfile실행
-
-export default app;
+app.listen(PORT, handleListening);
